2495929d03420dd52e0ec95374dd43bd59df1b27
Eugene Aronne <34140975+ejaronne@users.noreply.github.com>
2022-02-04 18:47:39 -0500

    Update and rename cms-ars-3.1-moderate-overlay.rb to cms-ars-3.1-low-overlay.rb

---
+++ controls/cms-ars-3.1-low-overlay.rb
@@ -0,0 +1,36 @@
+# encoding: utf-8
+
+include_controls "eks-cis-node-baseline" do
+  
+  control 'eks-cis-3.2.1' do
+    impact 0.0
+    desc 'caveat', 'This is Not Applicable since the related security control (AC-6) is not applied to this system categorization in CMS ARS 3.1'
+  end 
+  
+  control 'eks-cis-3.2.2' do
+    impact 0.0
+    desc 'caveat', 'This is Not Applicable since the related security control (AC-6) is not applied to this system categorization in CMS ARS 3.1'
+  end
+  
+  control 'eks-cis-3.2.3' do
+    impact 0.0
+    desc 'caveat', 'This is Not Applicable since the related security control (SC-8) is not applied to this system categorization in CMS ARS 3.1'
+  end
+  
+  control 'eks-cis-3.2.4' do
+    impact 0.0
+    desc 'caveat', 'This is Not Applicable since the related security control (CM-7 (1)) is not applied to this system categorization in CMS ARS 3.1'
+  end
+
+  control 'eks-cis-3.2.10' do
+    impact 0.0
+    desc 'caveat', 'This is Not Applicable since the related security control (SC-8) is not applied to this system categorization in CMS ARS 3.1'
+  end
+  
+  control 'eks-cis-3.2.11' do
+    impact 0.0
+    desc 'caveat', 'This is Not Applicable since the related security control (SC-8) is not applied to this system categorization in CMS ARS 3.1'
+  end  
+  
+end
+ 
--- controls/cms-ars-3.1-moderate-overlay.rb
+++
@@ -1,6 +0,0 @@
-# encoding: utf-8
-
-include_controls "eks-cis-node-baseline" do
-  
-end
- 
